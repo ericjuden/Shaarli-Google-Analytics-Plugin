b48968576d20ca07a3805f33dd2a2ce52a5ee915
葉家郡 <xnumtw@gmail.com>
2023-10-29 13:40:58 +0800

    Update google_analytics.php

--- google_analytics/google_analytics.php
+++ google_analytics/google_analytics.php
@@ -49,14 +49,13 @@ function hook_google_analytics_render_footer($data, $conf)
         return $data;
     }
 
-	$data['endofpage'][] = '<script>' .
-'  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' .
-'  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' .
-'  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' .
-'  })(window,document,"script","https://www.google-analytics.com/analytics.js","ga");' .
-'  ga("create", "'. $googleAnalyticsTrackingID .'", "auto");' .
-'  ga("send", "pageview");' .
-'</script>';
+	$data['endofpage'][] = '<script async src="https://www.googletagmanager.com/gtag/js?id=' . $googleAnalyticsTrackingID . '"></script>' .
+'	<script>'.
+'	window.dataLayer = window.dataLayer || [];'.
+'	function gtag(){dataLayer.push(arguments);}'.
+'	gtag("js", new Date());'.
+'	gtag("config", "' . $googleAnalyticsTrackingID . '");'.
+'	</script>';
 
 	return $data;
 }
